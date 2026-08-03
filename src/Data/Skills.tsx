2c78b5a7eb2035751b1017a3c1bfd5b80c175178
Batool Fatima <batoolfatima2135@gmail.com>
2026-08-03 16:56:30 +0200

    Added skills Data

---
+++ src/Data/Skills.tsx
@@ -0,0 +1,293 @@
+import {
+  SiHtml5,
+  SiCss,
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiNextdotjs,
+  SiRedux,
+  SiGreensock,
+  SiTailwindcss,
+  SiBootstrap,
+  SiMui,
+  SiAntdesign,
+  SiNodedotjs,
+  SiMysql,
+  SiPostgresql,
+  SiJest,
+  SiGit,
+  SiGithub,
+  SiJira,
+  SiConfluence,
+  SiEslint,
+  SiStylelint,
+  SiGooglechrome,
+} from "react-icons/si";
+
+export const frontendSkills = [
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiHtml5 color="white" />
+        <p className="text-sm font-medium">HTML5</p>
+      </div>
+    ),
+    title: "HTML5",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiCss color="white" /> <p className="text-sm font-medium">CSS3</p>
+      </div>
+    ),
+    title: "CSS3",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiJavascript color="white" />
+        <p className="text-sm font-medium">JavaScript</p>
+      </div>
+    ),
+    title: "JavaScript",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiTypescript color="white" />
+        <p className="text-sm font-medium">TypeScript</p>
+      </div>
+    ),
+    title: "TypeScript",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiReact color="white" />
+        <p className="text-sm font-medium">React</p>
+      </div>
+    ),
+    title: "React",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiNextdotjs color="white" />
+        <p className="text-sm font-medium">Next.js</p>
+      </div>
+    ),
+    title: "Next.js",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiRedux color="white" />
+        <p className="text-sm font-medium">Redux</p>
+      </div>
+    ),
+    title: "Redux",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiJest color="white" />
+        <p className="text-sm font-medium">Jest</p>
+      </div>
+    ),
+    title: "Jest",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <p className="text-xl font-bold">RTL</p>
+        <p className="text-sm font-medium">React Testing Library</p>
+      </div>
+    ),
+    title: "React Testing Library",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiGreensock color="white" />
+        <p className="text-sm font-medium">GSAP</p>
+      </div>
+    ),
+    title: "GSAP",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiTailwindcss color="white" />
+        <p className="text-sm font-medium">Tailwind CSS</p>
+      </div>
+    ),
+    title: "Tailwind CSS",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiBootstrap color="white" />
+        <p className="text-sm font-medium">Bootstrap</p>
+      </div>
+    ),
+    title: "Bootstrap",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiMui color="white" /> <p className="text-sm font-medium">MUI</p>
+      </div>
+    ),
+    title: "MUI",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiAntdesign color="white" />
+        <p className="text-sm font-medium">Ant Design</p>
+      </div>
+    ),
+    title: "Ant Design",
+  },
+];
+
+export const backendSkills = [
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <p className="text-xl font-bold">C#</p>
+        <p className="text-sm font-medium">C#</p>
+      </div>
+    ),
+    title: "C#",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <p className="text-xl font-bold">.NET</p>
+        <p className="text-sm font-medium">ASP.NET</p>
+      </div>
+    ),
+    title: "ASP.NET",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <p className="text-xl font-bold">ABP</p>
+        <p className="text-sm font-medium">ABP Framework</p>
+      </div>
+    ),
+    title: "ABP Framework",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiNodedotjs color="white" />
+        <p className="text-sm font-medium">Node.js</p>
+      </div>
+    ),
+    title: "Node.js",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <p className="text-xl font-bold">API</p>
+        <p className="text-sm font-medium">REST APIs</p>
+      </div>
+    ),
+    title: "REST APIs",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiMysql color="white" />
+        <p className="text-sm font-medium">MySQL</p>
+      </div>
+    ),
+    title: "MySQL",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiPostgresql color="white" />
+        <p className="text-sm font-medium">PostgreSQL</p>
+      </div>
+    ),
+    title: "PostgreSQL",
+  },
+];
+
+export const toolsSkills = [
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiGit color="white" />
+        <p className="text-sm font-medium">Git</p>
+      </div>
+    ),
+    title: "Git",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiGithub color="white" />
+        <p className="text-sm font-medium">GitHub</p>
+      </div>
+    ),
+    title: "GitHub",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiGooglechrome color="white" />
+        <p className="text-sm font-medium">Chrome DevTools</p>
+      </div>
+    ),
+    title: "Chrome DevTools",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiEslint color="white" />
+        <p className="text-sm font-medium">ESLint</p>
+      </div>
+    ),
+    title: "ESLint",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiStylelint color="white" />
+        <p className="text-sm font-medium">Stylelint</p>
+      </div>
+    ),
+    title: "Stylelint",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <p className="text-xl font-bold">Amazon</p>
+        <p className="text-sm font-medium">Web Services</p>
+      </div>
+    ),
+    title: "AWS",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiJira color="white" />
+        <p className="text-sm font-medium">Jira</p>
+      </div>
+    ),
+    title: "Jira",
+  },
+  {
+    node: (
+      <div className="flex flex-col items-center gap-2 text-white">
+        <SiConfluence color="white" />
+        <p className="text-sm font-medium">Confluence</p>
+      </div>
+    ),
+    title: "Confluence",
+  },
+];
