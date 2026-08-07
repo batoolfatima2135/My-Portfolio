fe2f295dec7c926542a0d711977bb37b94627fe6
Batool Fatima <batoolfatima2135@gmail.com>
2026-08-08 00:51:51 +0200

    Removed Electroborder ( was very slow on mobile) and added BorderGlow

--- src/sections/Achievements.tsx
+++ src/sections/Achievements.tsx
@@ -1,11 +1,11 @@
-import ElectroBorder from "@/components/lightswind/electro-border";
+import BorderGlow from "@/components/BorderGlow/BorderGlow";
 import ScrollFloat from "@/components/ScrollFloat/ScrollFloat";
 import ScrollReveal from "@/components/ScrollReveal/ScrollReveal";
 import { Trophy } from "lucide-react";
 
 export const Achievements: React.FC = () => {
   return (
-    <section>
+    <section className=" md:pt-20">
       <ScrollFloat
         animationDuration={1}
         ease="back.inOut(2)"
@@ -13,15 +13,25 @@ export const Achievements: React.FC = () => {
         scrollEnd="bottom bottom-=40%"
         stagger={0.03}
       >
-        ACHIEVEMENTS
+        Achievements
       </ScrollFloat>
-      <div className="md:w-7/8 sm:w-full px-1 md:px-8 lg:px-12 py-8 mx-auto">
+      <div className="w-full px-1 md:px-8 lg:px-12 md:py-8 mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 p-6">
           <div className="my-5">
-            <ElectroBorder borderColor="#f472b6">
-              <div className=" flex items-center gap-4 justify-center p-10">
-                <div className="flex h-16 w-16 items-center justify-center rounded-2xl bg-gradient-to-br from-yellow-400 via-amber-500 to-orange-500 text-white shadow-lg">
-                  <Trophy className="h-8 w-8" />
+            <BorderGlow
+              edgeSensitivity={8}
+              glowColor="40 80 80"
+              backgroundColor="#120F17"
+              borderRadius={16}
+              glowRadius={80}
+              glowIntensity={3}
+              coneSpread={34}
+              animated
+              colors={["#c084fc", "#f472b6", "#38bdf8"]}
+            >
+              <div className=" flex flex-col md:flex-row items-center gap-4 justify-center p-5 md:p-10">
+                <div className="flex h-10 w-10 md:h-16 md:w-16 items-center justify-center rounded-2xl bg-gradient-to-br from-yellow-400 via-amber-500 to-orange-500 text-white shadow-lg">
+                  <Trophy className="h-6  w-6 md:h-8 md:w-8" />
                 </div>
 
                 <ScrollFloat
@@ -68,11 +78,21 @@ export const Achievements: React.FC = () => {
                   />
                 </div>
               </div>
-            </ElectroBorder>
+            </BorderGlow>
           </div>
           <div className="my-5">
-            <ElectroBorder>
-              <div className=" flex items-center gap-4 justify-center p-10">
+            <BorderGlow
+              edgeSensitivity={8}
+              glowColor="40 80 80"
+              backgroundColor="#120F17"
+              borderRadius={16}
+              glowRadius={80}
+              glowIntensity={3}
+              coneSpread={34}
+              animated
+              colors={["#c084fc", "#f472b6", "#38bdf8"]}
+            >
+              <div className=" flex items-center  justify-center md:p-10">
                 <ScrollFloat
                   animationDuration={1}
                   ease="back.inOut(2)"
@@ -84,7 +104,7 @@ export const Achievements: React.FC = () => {
                   Leadership Program
                 </ScrollFloat>
               </div>
-              <div className="px-5">
+              <div className="md:px-5">
                 <ScrollReveal
                   baseOpacity={0.1}
                   enableBlur
@@ -93,23 +113,23 @@ export const Achievements: React.FC = () => {
                   textClassName="text-skills-xs"
                 >
                   Selected as the Engineering Department representative for
-                  Talverse's Future Leadership Program 2025. Strengthened
-                  leadership, communication, and presentation skills through
-                  peer collaboration, cross-functional teamwork, and delivering
-                  impactful presentations while gaining insights into effective
-                  leadership practices.
+                  Talverse's Future Leadership Program 2025. Strengthened and
+                  proved my leadership, communication, and presentation skills
+                  through peer collaboration, cross-functional teamwork, and
+                  delivering impactful presentations while gaining insights into
+                  effective leadership practices.
                 </ScrollReveal>
               </div>
-              <div className="grid grid-cols-1 gap-6 p-5 md:mt-12 sm:mt-5">
+              <div className="grid grid-cols-1 gap-6 p-5 md:mt-10 sm:mt-5">
                 <div className="rounded-2xl overflow-hidden w-7/8 mx-auto">
                   <img
                     src="./future-leadership-program.jpeg"
-                    alt="Award"
+                    alt="Certificate"
                     className="h-full w-full object-cover rounded-2xl"
                   />
                 </div>
               </div>
-            </ElectroBorder>
+            </BorderGlow>
           </div>
         </div>
       </div>
