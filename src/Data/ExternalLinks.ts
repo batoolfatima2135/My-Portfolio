8c1e27cb154ea0bf0d2592ce5d1ff73ba164bf38
Batool Fatima <batoolfatima2135@gmail.com>
2026-08-08 00:49:47 +0200

    Added external links

---
+++ src/Data/ExternalLinks.ts
@@ -0,0 +1,5 @@
+export const externalLinks = {
+  linkedIn: "https://www.linkedin.com/in/batoolfatima2135/",
+  gitHub: "https://github.com/batoolfatima2135",
+  cv: "/Batool-Fatima-Zaidi-CV.pdf",
+};
