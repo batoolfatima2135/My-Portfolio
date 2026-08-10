6c895ed30bd22a65c788a7b615de25b95f85ebbc
Batool Fatima <batoolfatima2135@gmail.com>
2026-08-11 00:26:28 +0200

    Added translations in Skill section

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -2,8 +2,10 @@ import ScrollFloat from "@/components/ScrollFloat/ScrollFloat";
 import ScrollReveal from "@/components/ScrollReveal/ScrollReveal";
 import { SkillCard } from "@/components/SkillCard";
 import { backendSkills, frontendSkills, toolsSkills } from "@/Data/SkillsData";
+import { useTranslation } from "react-i18next";
 
 export const Skills: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <section className="w-full px-4 md:px-8 lg:px-16 pt-10 md:pt-30">
       <ScrollFloat
@@ -13,7 +15,7 @@ export const Skills: React.FC = () => {
         scrollEnd="bottom bottom-=40%"
         stagger={0.03}
       >
-        Skills
+        {t("skills.title")}
       </ScrollFloat>
       <div className="md:mx-20">
         <ScrollReveal
@@ -22,20 +24,17 @@ export const Skills: React.FC = () => {
           baseRotation={3}
           blurStrength={4}
         >
-          Skilled in building modern, responsive, and scalable web applications
-          using React, Next.js, TypeScript, C#, ASP.NET, and Node.js.
-          Experienced with REST APIs, SQL databases, testing, CI/CD, and Agile
-          development practices to deliver high-quality software.
+          {t("skills.description")}
         </ScrollReveal>
       </div>
 
       <div className="mt-12 grid grid-cols-1 md:grid-cols-2 gap-8">
-        <SkillCard logos={frontendSkills} title="Frontend" />
-        <SkillCard logos={backendSkills} title="Backend" />
+        <SkillCard logos={frontendSkills} title={t("skills.frontend")} />
+        <SkillCard logos={backendSkills} title={t("skills.backend")} />
 
         <div className="md:col-span-2 flex justify-center">
           <div className="w-full md:w-[calc(60%-1rem)]">
-            <SkillCard logos={toolsSkills} title="Tools & DevOps" />
+            <SkillCard logos={toolsSkills} title={t("skills.tools")} />
           </div>
         </div>
       </div>
