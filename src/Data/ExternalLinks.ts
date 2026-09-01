2bcb789c82c3a240511c7cdf309a7d5baef96d99
Batool Fatima <batoolfatima2135@gmail.com>
2026-09-01 21:18:54 +0200

    updated CV links

--- src/Data/ExternalLinks.ts
+++ src/Data/ExternalLinks.ts
@@ -1,8 +1,11 @@
+import GermanCV from "@/assets/Batool-Fatima-Zaidi-Lebenslauf.pdf";
+import EnglishCV from "@/assets/Batool-Fatima-Zaidi-CV.pdf";
+
 export const externalLinks = {
   linkedIn: "https://www.linkedin.com/in/batoolfatima2135/",
   gitHub: "https://github.com/batoolfatima2135",
   cv: {
-    en: "/Batool-Fatima-Zaidi-CV.pdf",
-    de: "/Batool-Fatima-Zaidi-Lebenslauf.pdf",
+    en: EnglishCV,
+    de: GermanCV,
   },
 };
